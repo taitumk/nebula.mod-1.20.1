4396dfaf13c8e266d529d0af8f856ee1163365ad
HolyPotato <taitumkennedy@gmail.com>
2023-11-06 20:26:56 +1100

    test commit

---
+++ src/main/java/net/nebula/mod/NebulaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nebula.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NebulaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/nebula/mod/NebulaModClient.java
@@ -0,0 +1,10 @@
+package net.nebula.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NebulaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
